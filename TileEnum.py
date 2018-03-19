cf7a3944e5ee98e17765645140a0f23afadd8777
orensultan <orens2806@gmail.com>
2018-03-19 16:29:57 +1100

    firest v2

---
+++ TileEnum.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class TileEnum(Enum):
+    WHITE_PIECE = 0
+    BLACK_PIECE = 1
+    EMPTY_TILE = 2
+    CORNER_TILE = 3
+
--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from WrongNumberOfTilesException import WrongNumberOfTilesException
 from WrongSymbolException import WrongSymbolException
 from WrongInput import WrongInput
 from Node import Node
+from TileEnum import TileEnum
 import copy
+
 
 NUMBER_OF_ROWS = 8
 ROW_LEN = 8
@@ -30,15 +32,15 @@ def main():
                 elif i == 1:
                     row_input = '- - - - - - - -'
                 elif i == 2:
-                    row_input = '- - - - - O - -'
+                    row_input = '- - - - - O O -'
                 elif i == 3:
                     row_input = '- - - - @ O - -'
                 elif i == 4:
-                    row_input = '- - - - - - O -'
+                    row_input = '- - - - - - - -'
                 elif i == 5:
-                    row_input = '- - - - - O @ -'
+                    row_input = '- - - - - O - -'
                 elif i == 6:
-                    row_input = '- - - - - - - @'
+                    row_input = '- - - - @ - @ @'
                 else:
                     row_input = 'X - - - - - - X'
             else:
@@ -49,17 +51,13 @@ def main():
             else:
                 for tile in row_input:
                     if tile == WHITE_PIECE_SYM:
-                        white_piece = PieceTile((counter_of_tiles_in_row, i), PieceTile.WHITE_COLOR)
-                        row.append(white_piece)
+                        row.append(TileEnum.WHITE_PIECE)
                     elif tile == BLACK_PIECE_SYM:
-                        black_piece = PieceTile((counter_of_tiles_in_row, i), PieceTile.BLACK_COLOR)
-                        row.append(black_piece)
+                        row.append(TileEnum.BLACK_PIECE)
                     elif tile == CORNER_SYM:
-                        corner = CornerTile((counter_of_tiles_in_row, i))
-                        row.append(corner)
+                        row.append(TileEnum.CORNER_TILE)
                     elif tile == EMPTY_SYM:
-                        empty = EmptyTile((counter_of_tiles_in_row, i))
-                        row.append(empty)
+                        row.append(TileEnum.EMPTY_TILE)
                     elif tile == SPACE:
                         continue
                     else:
@@ -74,7 +72,6 @@ def main():
             legal_white_moves , legal_black_moves = count_moves()
             print(legal_white_moves, legal_black_moves)
         else:
-            print(board)
             build_search_tree()
 
     except WrongNumberOfTilesException:
@@ -86,31 +83,27 @@ def main():
 
 def build_search_tree():
     node = Node(board)
-
-
-    for row in board:
-        for tile in row:
-            if isinstance(tile, PieceTile):
-                if tile.get_color() == PieceTile.WHITE_COLOR:
-                    count, legal_new_pos = count_legal_moves_pos(tile.get_location())
-                    for new_pos in legal_new_pos:
-                        create_child(tile.get_location(), new_pos)
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == TileEnum.WHITE_PIECE:
+                count, legal_new_pos = count_legal_moves_pos((i, j))
+                for new_pos in legal_new_pos:
+                    create_child((i, j), new_pos)
 
 
 def create_child(old_pos, new_pos):
-    old_pos_row = old_pos[1]
-    old_pos_col = old_pos[0]
-    new_pos_row = new_pos[1]
-    new_pos_col = new_pos[0]
+    old_pos_row = old_pos[0]
+    old_pos_col = old_pos[1]
+    new_pos_row = new_pos[0]
+    new_pos_col = new_pos[1]
 
     copy_board = copy.deepcopy(board)
 
     temp_tile = copy_board[new_pos_row][new_pos_col]
+
     copy_board[new_pos_row][new_pos_col] = copy_board[old_pos_row][old_pos_col]
     copy_board[old_pos_row][old_pos_col] = temp_tile
 
-    copy_board[old_pos_row][old_pos_col].set_location((old_pos_row, old_pos_col))
-    copy_board[new_pos_row][new_pos_col].set_location((new_pos_row, new_pos_col))
 
     print("original board old loc")
     print(old_pos_row)
@@ -133,15 +126,15 @@ def create_child(old_pos, new_pos):
 def count_moves():
     count_white_moves = 0
     count_black_moves = 0
-    for row in board:
-        for tile in row:
-            if isinstance(tile, PieceTile):
-                if tile.get_color() == PieceTile.WHITE_COLOR:
-                    count, move_new_pos = count_legal_moves_pos(tile.get_location())
-                    count_white_moves += count
-                else:
-                    count, move_new_pos = count_legal_moves_pos(tile.get_location())
-                    count_black_moves += count
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == TileEnum.WHITE_PIECE:
+                count, move_new_pos = count_legal_moves_pos((i, j))
+                count_white_moves += count
+            elif board[i][j] == TileEnum.BLACK_PIECE:
+                count, move_new_pos = count_legal_moves_pos((i, j))
+                count_black_moves += count
+
     return count_white_moves, count_black_moves
 
 
@@ -149,32 +142,32 @@ def check_left_move(loc):
 
     left_move_new_pos = []
     legal_moves = 0
-    row = loc[1]
-    col = loc[0]
+    row = loc[0]
+    col = loc[1]
     if col > 0:
-        if isinstance(board[row][col - 1], EmptyTile):
-            left_move_new_pos.append((col - 1, row))
+        if board[row][col - 1] == TileEnum.EMPTY_TILE:
+            left_move_new_pos.append((row, col - 1))
             legal_moves += 1
-        elif isinstance(board[row][col - 1], PieceTile):
+        elif board[row][col - 1] in (TileEnum.BLACK_PIECE, TileEnum.WHITE_PIECE):
             if col - 1 > 0:
-                if isinstance(board[row][col - 2], EmptyTile):
-                    left_move_new_pos.append((col - 2, row))
+                if board[row][col - 2] == TileEnum.EMPTY_TILE:
+                    left_move_new_pos.append((row, col - 2))
                     legal_moves += 1
     return legal_moves, left_move_new_pos
 
 def check_up_move(loc):
     up_move_new_pos = []
     legal_moves = 0
-    row = loc[1]
-    col = loc[0]
+    row = loc[0]
+    col = loc[1]
     if row > 0:
-        if isinstance(board[row - 1][col], EmptyTile):
-            up_move_new_pos.append((col, row - 1))
+        if board[row - 1][col] == TileEnum.EMPTY_TILE:
+            up_move_new_pos.append((row - 1, col))
             legal_moves += 1
-        elif isinstance(board[row - 1][col], PieceTile):
+        elif board[row - 1][col] in (TileEnum.BLACK_PIECE, TileEnum.WHITE_PIECE):
             if row - 1 > 0:
-                if isinstance(board[row - 2][col], EmptyTile):
-                    up_move_new_pos.append((col, row - 2))
+                if board[row - 2][col] == TileEnum.EMPTY_TILE:
+                    up_move_new_pos.append((row - 2, col))
                     legal_moves += 1
     return legal_moves, up_move_new_pos
 
@@ -183,33 +176,33 @@ def check_right_move(loc):
 
     right_move_new_pos = []
     legal_moves = 0
-    row = loc[1]
-    col = loc[0]
+    row = loc[0]
+    col = loc[1]
     if col < ROW_LEN - 1:
-        if isinstance(board[row][col + 1], EmptyTile):
-            right_move_new_pos.append((col + 1, row))
+        if board[row][col + 1] == TileEnum.EMPTY_TILE:
+            right_move_new_pos.append((row, col + 1))
             legal_moves += 1
-        elif isinstance(board[row][col + 1], PieceTile):
+        elif board[row][col + 1] in (TileEnum.BLACK_PIECE, TileEnum.WHITE_PIECE):
             if col + 1 < ROW_LEN - 1:
-                if isinstance(board[row][col + 2], EmptyTile):
-                    right_move_new_pos.append((col + 2, row))
+                if board[row][col + 2] == TileEnum.EMPTY_TILE:
+                    right_move_new_pos.append((row, col + 2))
                     legal_moves += 1
     return legal_moves, right_move_new_pos
 
 def check_down_move(loc):
     down_move_new_pos = []
     legal_moves = 0
-    row = loc[1]
-    col = loc[0]
+    row = loc[0]
+    col = loc[1]
     if row < ROW_LEN - 1:
-        if isinstance(board[row + 1][col], EmptyTile):
+        if board[row + 1][col] == TileEnum.EMPTY_TILE:
             legal_moves += 1
-            down_move_new_pos.append((col, row + 1))
-        elif isinstance(board[row + 1][col], PieceTile):
+            down_move_new_pos.append((row + 1, col))
+        elif board[row + 1][col] in (TileEnum.BLACK_PIECE, TileEnum.WHITE_PIECE):
             if row + 1 < ROW_LEN - 1:
-                if isinstance(board[row + 2][col], EmptyTile):
+                if board[row + 2][col] == TileEnum.EMPTY_TILE:
                     legal_moves += 1
-                    down_move_new_pos.append((col, row + 1))
+                    down_move_new_pos.append((row + 2, col))
     return legal_moves, down_move_new_pos
 
 def count_legal_moves_pos(loc):
